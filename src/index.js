d8cc69d04ceb10c5f0d7891219cbbbd4fd3e4780
bolotbekMM <bolotbek.muratov@gmail.com>
2022-06-25 19:30:17 +0600

    finished project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./sass/index.scss";
-import header from "./js/header";
+import { header } from "./js/header";
 import { Validation } from "./js/validation";
 import { modal } from "./js/modal";
 import { servisesValidation } from "./js/servisesValidation";
 
+header();
 Validation();
 modal();
 servisesValidation();
